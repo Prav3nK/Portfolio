1df1a7808956e339315d7311f0676f6c6db1ed22
Prav3nK <praveen259367@gmail.com>
2024-04-27 12:25:42 +1000

    all

--- portfolio-app/src/components/Contact/contact.js
+++ portfolio-app/src/components/Contact/contact.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from 'axios';
 import wal from "../../assets/walmart.png"
 import mic from "../../assets/microsoft.png"
 import adobe from "../../assets/adobe.png"
@@ -11,6 +11,39 @@ import youTubeIcon from "../../assets/youtube.png"
 import instagramIc from "../../assets/instagram.png"
 
 const Conctact = () =>{
+        const [name,setName] = useState('');
+        const [email,setEmail] = useState('');
+        const [message, setMessage] = useState('');
+
+        const handleSubmit = async (e) => {
+        e.preventDefault();
+        const serviceId = 'service_xw8v5lm';
+        const publickey = 'F5otaVPDyO_UXTzTi';
+        const templateId ='template_14k70ei';
+
+        const data= {
+           service_id : serviceId,
+           template_id : templateId,
+           user_id : publickey,
+           template_params : {
+            from_name : name,
+            from_email : email,
+            message : message,
+            to_name : 'Praveen Kumar'
+           }
+           }
+
+           try{
+            const res = await axios.post("https://api.emailjs.com/api/v1.0/email/send",data)
+            console.log(res.data)
+            setName('');
+            setEmail('');
+            setMessage('');
+           } catch (error){
+            console.error(error);
+           }
+       }
+
     return (
         <section id="contactsec">
             <div id="clients">
@@ -28,11 +61,31 @@ const Conctact = () =>{
             <div id="contact">
                 <h1 className="sectionTitle"> Contact Me</h1>   
                 <span className="sectionDes">Please fill out the form below to discuss any work opportunies.</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your name" ></input>
-                    <input type="email" className="em" placeholder="Email"></input>
-                    <textarea className="text" name="message" rows ="5" placeholder="Enter Message"></textarea>
-                    <button type="submit" className="subBtn" value="send">Submit</button>
+                
+                <form className="contactForm" onSubmit={handleSubmit}>
+                    <input
+                     type="text" 
+                     className="name" 
+                     placeholder="Your name"  
+                     value ={name}
+                     onChange  ={(e)=>setName(e.target.value)}
+                     />                     
+                    <input
+                     type="email" 
+                     className="em" 
+                     placeholder="Email" 
+                     value ={email} 
+                    onChange  ={(e)=>setEmail(e.target.value)}
+                    />
+                    <textarea 
+                    className="text"
+                     name="message" 
+                     rows ="5" 
+                     placeholder="Enter Message" 
+                     value={message}
+                     onChange  ={(e)=>setMessage(e.target.value)}
+                      />
+                    <button type="submit" className="subBtn">Submit</button>
                     <div className="links">
                         <img src={facebookIcon} alt="" className="linkimg"/>
                         <img src={twitter} alt="" className="linkimg"/>
--- portfolio-app/src/components/Navbar/Navbar.js
+++ portfolio-app/src/components/Navbar/Navbar.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css' 
 import logo from "../../assets/logo.png"
 import {Link } from 'react-scroll';
 import contactImg from "../../assets/contact.png"
+import menu from "../../assets/menu.png"
 
 
 const Navbar =() =>{
+   const [showMenu, setShowMenu] = useState(false)
     return (
        <nav className="navbar">
         <img src={logo} alt="Logo" className="logo"/>
         <div className="desktopMenu">
-        <Link className="desktopMenuListItem">Home</Link>
-        <Link className="desktopMenuListItem">About</Link>
-        <Link className="desktopMenuListItem">Projects</Link>
-        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link  activeClass="active" to="first" spy={true} smooth={true}  offset={-50} duration ={500} className="desktopMenuListItem">Home</Link>
+        <Link  activeClass="active" to="skll" spy={true} smooth={true}  offset={-50} duration ={500}className="desktopMenuListItem">About</Link>
+        <Link  activeClass="active" to="projects" spy={true} smooth={true}  offset={-50} duration ={500}className="desktopMenuListItem">Portfolio</Link>
+        <Link  activeClass="active" to="contactsec" spy={true} smooth={true}  offset={-500} duration ={500}className="desktopMenuListItem">Projects</Link>
         </div>
-         <button class="desktopMenuBtn">
+         <button class="desktopMenuBtn" onClick={()=>{
+            document.getElementById("contact").scrollIntoView();
+         }}>
             <img src={contactImg}alt="" className="desktopMenuImg" /> Contact Me
          </button>
+         <img src={menu} alt="Logo" className="mobmenu" onClick={()=>setShowMenu(!showMenu)}/>
+        <div className="navMenu" style={{display: showMenu? 'flex': 'none'}}>
+        <Link  activeClass="active" to="first" spy={true} smooth={true}  offset={-50} duration ={500} className="ListItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link  activeClass="active" to="skll" spy={true} smooth={true}  offset={-50} duration ={500}className="ListItem" onClick={()=>setShowMenu(false)}>About</Link>
+        <Link  activeClass="active" to="projects" spy={true} smooth={true}  offset={-50} duration ={500}className="ListItem"onClick={()=>setShowMenu(false)}>Portfolio</Link>
+        <Link  activeClass="active" to="contactsec" spy={true} smooth={true}  offset={-500} duration ={500}className="ListItem"onClick={()=>setShowMenu(false)}>Projects</Link>
+        <Link  activeClass="active" to="contact" spy={true} smooth={true}  offset={-500} duration ={500}className="ListItem"onClick={()=>setShowMenu(false)}>Contact</Link>
+        </div>
        </nav> 
     )
 }
